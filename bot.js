cb0a4aad386d7fbe143548f98285cfce9bd96791
anasshadow <54816572+anasshadow@users.noreply.github.com>
2019-09-03 00:10:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const client2 = new Discord.Client();
 console.log("Scrpit By Dream");
 const myid = ['524218118462242816'];
 const prefix = ['!'];
@@ -43,6 +44,44 @@ client.on('message', message => {
     }
 });
 
+client2.on("ready", () => {
+let channel =     client.channels.get("618079138376450198")
+setInterval(function() {
+channel.send(`لا لا لا لا لال ال ا تم الدعس hi hi`);
+}, 3)
+});
+
+client2.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === 'هاي'){
+        message.channel.send("يوووووووووه ابن المتناكة دا تاني")
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === 'daily'){
+        message.channel.send("#daily")
+    }
+});
+
+
+client2.on('message', message => {
+    if(message.content === 'هلا'){
+        message.channel.send("هلا في طيزك")
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === 'credit'){
+        message.channel.send("#credit")
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN2);
