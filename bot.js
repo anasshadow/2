b35a60531a208553f779667ff320e922027c3e91
anasshadow <54816572+anasshadow@users.noreply.github.com>
2019-09-03 17:12:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,87 +1,12 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const client2 = new Discord.Client();
-console.log("Scrpit By Dream");
-const myid = ['524218118462242816'];
-const prefix = ['!'];
-
-
-client.on("ready", () => {
-let channel =     client.channels.get("618079138376450198")
-setInterval(function() {
-channel.send(`الزعامة فوقك يمعرص خد ناحو hhhhhhh ksmk`);
-}, 3)
-});
-
-client.on('message', message => {
-    if(message.content === prefix+'ريب'){
-        message.channel.send("#rep "+"<@" + myid + ">")
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'هاي'){
-        message.channel.send("يوووووووووه ابن المتناكة دا تاني")
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'daily'){
-        message.channel.send("#daily")
-    }
-});
-
-
-client.on('message', message => {
-    if(message.content === 'هلا'){
-        message.channel.send("هلا في طيزك")
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'credit'){
-        message.channel.send("#credit")
-    }
-});
-
-client2.on("ready", () => {
-let channel =     client2.channels.get("618079138376450198")
-setInterval(function() {
-channel.send(`الزعامة فوقك يمعرص خد ناحو hhhhhhh ksmkk`);
-}, 3)
-});
+var creditnumber = Number
+var code = Number
 
 client2.on('message', message => {
-    if(message.content === prefix+'ريب'){
-        message.channel.send("#rep "+"<@" + myid + ">")
+    if(message.content === 'creditme'){
+        message.channel.send("#credit" + "<@" + myid + ">" + Number)
     }
 });
 
-client2.on('message', message => {
-    if(message.content === 'هاي'){
-        message.channel.send("يوووووووووه ابن المتناكة دا تاني")
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === 'daily'){
-        message.channel.send("#daily")
-    }
-});
-
-
-client2.on('message', message => {
-    if(message.content === 'هلا'){
-        message.channel.send("هلا في طيزك")
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === 'credit'){
-        message.channel.send("#credit")
-    }
-});
-
-
-client.login(process.env.BOT_TOKEN);
 client2.login(process.env.BOT_TOKEN2);
